0cc2c7fa6bb8bc5d01dc3a5371e39c2a1a9bf2c2
Christopher EnyTC <chris@enytc.com>
2016-08-10 22:55:49 -0300

    Update voice endpoint

--- endpoints/voice.rb
+++ endpoints/voice.rb
@@ -1,34 +1,32 @@
 # encoding: utf-8
+# frozen_string_literal: true
 
 class PhonnyApplication < Sinatra::Application
+  post '/voice' do
+    from = /^client/ =~ params['From'] ? nil : params['From']
 
-	post '/voice' do
-		from = params['From']
+    response = Twilio::TwiML::Response.new do |r|
+      r.Dial callerId: from, timeout: 120, record: true, action: '/voice/handler' do |d|
+        d.Number Config.phone_number
+      end
+    end
 
-		response = Twilio::TwiML::Response.new do |r|
-			r.Dial from: from, callerId: from, timeout: 120, record: true, action: '/voice/handler' do |d|
-				d.Number Config.phone_number
-			end
-		end
+    response.text
+  end
 
-		response.text
-	end
+  post '/voice/handler' do
+    status = params['DialCallStatus']
+    id = params['DialCallSid']
+    duration = params['DialCallDuration']
+    voice_url = params['RecordingUrl']
 
-	post '/voice/handler' do
-		status = params['DialCallStatus']
-		id = params['DialCallSid']
-		duration = params['DialCallDuration']
-		voice_url = params['RecordingUrl']
+    puts "ID => #{id} => Call duration => #{duration} => Status => #{status}"
+    puts "Voice record => #{voice_url}" if voice_url
 
-		puts "ID => #{id} => Call duration => #{duration} => Status => #{status}"
-		puts "Voice record => #{voice_url}" if voice_url
+    response = Twilio::TwiML::Response.new do |r|
+      r.Say 'Goodbye.', voice: 'alice', language: 'en-GB'
+    end
 
-		response = Twilio::TwiML::Response.new do |r|
-			r.Say 'Goodbye.', voice: 'alice', language: 'en-GB'
-		end
-
-		response.text
-	end
-
+    response.text
+  end
 end
-
